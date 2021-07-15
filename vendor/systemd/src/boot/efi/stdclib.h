5707d4543d02b734fcfe56f12e83307b02f1bfcb
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2021-07-15 13:47:59 +0200

    Define some std C library functions
    Needed as we are building a PE+ binary for which we do not have
    libstdc.

---
+++ vendor/systemd/src/boot/efi/stdclib.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier:  */
+#pragma once
+
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+void *memchr(const void *s, int c, size_t n);
+size_t strlen(const char *s);
+size_t strnlen(const char *s, size_t maxlen);
+char *strchr(const char *s, int c);
+char *strrchr(const char *s, int c);
+unsigned long int strtoul(const char *nptr, char **endptr, int base);
