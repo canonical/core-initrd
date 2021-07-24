9cb18de156b2395646ad382f238c55e14e8bcc3c
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2021-07-24 11:09:23 +0200

    Some script clean-ups

--- vendor/systemd/src/boot/efi/fix-pe-stub.py
+++ vendor/systemd/src/boot/efi/fix-pe-stub.py
@@ -13,6 +13,8 @@ off_fhead_NumberOfSections = 2
 off_fhead_NumberOfSymbols = 12
 # Offsets inside optional headers
 off_opth_SizeOfInitializedData = 8
+off_opth_SectionAlignment = 32
+off_opth_FileAlignment = 36
 off_opth_SizeOfImage = 56
 off_opth_NumberOfRvaAndSizes = 108
 # Offsets inside section header
@@ -22,39 +24,42 @@ off_secth_VirtualAddress = 12
 off_secth_SizeOfRawData = 16
 
 
-def align_to_nbits(address, bits):
-    mask = (1 << bits) - 1
-    if address & mask != 0:
-        address = (address >> bits) << bits
-        address += 1 << bits
+def align_to_size(address, size):
+    rest = address % size
+    if rest:
+        address -= rest
+        address += size
     return address
 
 
 def fix_aarch64_pe_stub():
-    bits_section_align = 12
-    section_align = 0x1000
-    bits_file_align = 9
     with open("stub", "r+b") as stub:
 
         stub.seek(off_pe_pointer)
-        pe_off = int.from_bytes(stub.read(4), byteorder='little')
+        pe_off = int.from_bytes(stub.read(4), 'little')
         print('Offset to PE header: 0x{:x}'.format(pe_off))
 
         stub.seek(pe_off + sz_signature + off_fhead_NumberOfSections)
-        num_sect = int.from_bytes(stub.read(2), byteorder='little')
+        num_sect = int.from_bytes(stub.read(2), 'little')
         print('Num sect {}'.format(num_sect))
 
         # Set number of symbols to 0 (gnu-efi bug?)
         stub.seek(pe_off + sz_signature + off_fhead_NumberOfSymbols)
         stub.write(bytearray(4))
 
-        # Set load size and VMA for .linux and .initrd
+        # Get alignment values
+        stub.seek(pe_off + sz_signature + sz_coff_fhead +
+                  off_opth_SectionAlignment)
+        section_align = int.from_bytes(stub.read(4), 'little')
+        file_align = int.from_bytes(stub.read(4), 'little')
+
         stub.seek(pe_off + sz_signature + sz_coff_fhead +
                   off_opth_NumberOfRvaAndSizes)
-        num_dirs = int.from_bytes(stub.read(4), byteorder='little')
+        num_dirs = int.from_bytes(stub.read(4), 'little')
         sect_off = (pe_off + sz_signature + sz_coff_fhead + sz_opt_head +
                     8*num_dirs)
 
+        # Set load size and VMA for .linux and .initrd
         # We assume the order of sections is
         # .test, .data, .linux, .initrd
         data_sz = 0
@@ -70,25 +75,24 @@ def fix_aarch64_pe_stub():
 
             if sect_name == b'.data\x00\x00\x00':
                 stub.seek(sect_off + off_secth_VirtualSize)
-                data_sz = int.from_bytes(stub.read(4), byteorder='little')
-                data_va = int.from_bytes(stub.read(4), byteorder='little')
+                data_sz = int.from_bytes(stub.read(4), 'little')
+                data_va = int.from_bytes(stub.read(4), 'little')
                 disk_data += data_sz
-                linux_va = data_va + align_to_nbits(data_sz,
-                                                    bits_section_align)
+                linux_va = data_va + align_to_size(data_sz, section_align)
 
             if sect_name == b'.linux\x00\x00':
                 stub.seek(sect_off + off_secth_SizeOfRawData)
-                raw_linux_sz = int.from_bytes(stub.read(4), byteorder='little')
+                raw_linux_sz = int.from_bytes(stub.read(4), 'little')
                 stub.seek(sect_off + off_secth_VirtualSize)
                 stub.write(raw_linux_sz.to_bytes(4, 'little', signed=False))
                 stub.write(linux_va.to_bytes(4, 'little', signed=False))
 
-                disk_size = align_to_nbits(raw_linux_sz, bits_file_align)
+                disk_size = align_to_size(raw_linux_sz, file_align)
                 stub.write(disk_size.to_bytes(4, 'little', signed=False))
                 disk_data += disk_size
 
-                initrd_va = linux_va + align_to_nbits(raw_linux_sz,
-                                                      bits_section_align)
+                initrd_va = linux_va + align_to_size(raw_linux_sz,
+                                                     section_align)
 
                 print('Raw linux {:x}'.format(raw_linux_sz))
 
@@ -99,7 +103,7 @@ def fix_aarch64_pe_stub():
                 stub.write(raw_initrd_sz.to_bytes(4, 'little', signed=False))
                 stub.write(initrd_va.to_bytes(4, 'little', signed=False))
 
-                disk_size = align_to_nbits(raw_initrd_sz, bits_file_align)
+                disk_size = align_to_size(raw_initrd_sz, file_align)
                 stub.write(disk_size.to_bytes(4, 'little', signed=False))
                 disk_data += disk_size
 
@@ -112,7 +116,7 @@ def fix_aarch64_pe_stub():
 
         # Size in memory (page added for headers)
         image_sz = (initrd_va - vma_start +
-                    align_to_nbits(raw_initrd_sz, bits_section_align) +
+                    align_to_size(raw_initrd_sz, section_align) +
                     section_align)
         stub.seek(pe_off + sz_signature + sz_coff_fhead + off_opth_SizeOfImage)
         stub.write(image_sz.to_bytes(4, 'little', signed=False))
