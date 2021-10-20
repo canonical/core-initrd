2f71a215e171b0d057af617a9973ebb1b3499e8b
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2021-10-20 15:54:58 +0200

    load-drm-modules.sh: switch modules folder only when needed

--- factory/usr/bin/load-drm-module.sh
+++ factory/usr/bin/load-drm-module.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -ex
 
-ln -s /sbin/kmod /sbin/modprobe
-mv /lib/modules /lib/modules-initrd
-ln -s /run/mnt/kernel/lib/modules/ /lib/
-
 find /sys/devices/pci* -name boot_vga |
     while read -r boot_vga_f; do
         grep -q 1 "$boot_vga_f" || continue
         dev_d=${boot_vga_f%/boot_vga}
         modalias=$(cat "$dev_d"/modalias)
+
+        ln -s /sbin/kmod /sbin/modprobe
+        mv /lib/modules /lib/modules-initrd
+        ln -s /run/mnt/kernel/lib/modules/ /lib/
         modprobe "$modalias"
         break
     done
